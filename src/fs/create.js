cad2a8fa04d8d3719436d3c7120b7a0023c23408
Artem Karlov <ar.karlov@gmail.com>
2024-10-01 23:39:58 +0200

    feat: add file creating

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,15 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(import.meta.dirname, "files", "fresh.txt");
+  const content = "I am fresh and young";
+
+  try {
+    await fs.writeFile(filePath, content, { flag: "wx" });
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await create();
