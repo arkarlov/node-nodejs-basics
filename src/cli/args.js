238e56152061a42fe40b9452a67b518c76837db4
Artem Karlov <ar.karlov@gmail.com>
2024-10-02 23:26:56 +0200

    feat: implement cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
 const parseArgs = () => {
-    // Write your code here 
+  const parsedArgs = process.argv.slice(2).reduce((res, arg, index, args) => {
+    const isKey = (a) => a.startsWith("-");
+
+    if (!isKey(arg)) return res;
+
+    const key = arg.replace(/^-+/, "");
+    const next = args[index + 1];
+    res[key] = next && !isKey(next) ? next : true;
+
+    return res;
+  }, {});
+
+  console.log(
+    Object.entries(parsedArgs)
+      .map(([key, value]) => `${key} is ${value}`)
+      .join(", ")
+  );
 };
 
 parseArgs();
