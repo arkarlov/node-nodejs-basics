70bfc1a06127e3c9c272aeff822dc7f843845a99
Artem Karlov <ar.karlov@gmail.com>
2024-10-03 21:11:33 +0200

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import crypto from "node:crypto";
+import fs from "node:fs";
+import path from "node:path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = crypto.createHash("sha256");
+
+  const readStream = fs.createReadStream(
+    path.join(import.meta.dirname, "files", "fileToCalculateHashFor.txt")
+  );
+
+  readStream.on("readable", () => {
+    const data = readStream.read();
+
+    if (data) {
+      hash.update(data);
+    } else {
+      console.log(hash.digest("hex"));
+    }
+  });
 };
 
 await calculateHash();
